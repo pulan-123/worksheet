a77f8ba2aa0c659239bf4f85f3efdd8f2e891ce3
pulan-123 <2103493125@qq.com>
2026-05-12 15:09:32 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,19 @@
-const CACHE_NAME = 'timetable-app-v1';
+const CACHE_NAME = 'timetable-v2';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon.svg'
 ];
 
-// 安装 Service Worker
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      // 逐个添加以防万一某个失败不影响整体
-      return Promise.allSettled(
-        ASSETS.map(asset => cache.add(asset))
-      );
-    })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-// 激活 Service Worker
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
-});
-
-// 拦截请求
-self.addEventListener('fetch', (event) => {
-  // 只拦截同源请求或特定的资源
-  if (event.request.url.startsWith('http')) {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  }
 });
